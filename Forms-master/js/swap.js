d8ad59261f56a466426f6b1fb38d6ea28ae94889
Disnasudheer <119562836+Disnasudheer@users.noreply.github.com>
2023-10-12 11:16:28 +0530

    Delete Forms-master directory

--- Forms-master/js/swap.js
+++
@@ -1,5 +0,0 @@
-function swap(){
-    var content = document.getElementById('para1').innerHTML
-   document.getElementById('para').innerHTML = document.getElementById('paraa1').innerHTML
-   document.getElementById('para').innerHTML = content
-}
